31464f2b4d0eed5734575757c90d76ee906ab34c
Rafaela Oliveira <rafaela62242406@alunos.sc.senac.br>
2026-03-17 20:51:55 -0300

    adicionei mudanças na lista

--- Listafrutas.py
+++ Listafrutas.py
@@ -1,4 +1,4 @@
-git afrutas = ["maçã", "banana", "uva"]
+frutas = ["maçã", "banana", "uva"]
 numeros = [1, 2, 3, 4]
 
 #Acessando elementos
@@ -10,8 +10,8 @@ frutas[1] = "banana-nanica"
 print("Após alterar:", frutas)
 
 #Adicionando elementos
-frutas.append("morango")    #adicionada ao final
-frutas.insert(1, "melancia")    #adicionada na posição 1
+frutas.append("melancia")    #adicionada ao final
+frutas.insert(1, "morango")    #adicionada na posição 1
 print("Após adicionar:", frutas)
 
 #Removendo elementos
@@ -19,3 +19,12 @@ frutas.remove("maçã")         #remove pelo valor
 ultima = frutas.pop()       #remove e retorna o último
 print("Após remover 'maça' e pop();", frutas, "|Última removida:", ultima)
 
+#Tamanho (quantidade de elementos)
+print("Tamanho da lista 'frutas':", len(frutas))
+
+#Fatiamento (slicing)
+print("Fatiamento [0:2]:", frutas[0:2]
+)
+
+#Verificar se um item está na lista
+print("Tem 'morango'?", 'morango' in frutas)
